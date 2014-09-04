c5f9ef22b68dcaa768865efcb29f660affbf5e64
Spencer <spencer.mize@ambassador-enterpirses.com>
2014-09-04 16:16:35 -0400

    keep tweaking

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var $j = jQuery.noConflict();
 $j(function() {
-	$j(".post-list li h1").fitText(.9);
-	$j(".post-list li h2").fitText(1.46);
+	//$j(".post-list li h1").fitText(.5);
+	//$j(".post-list li h2").fitText(.8);
 });
